e89855793ea96905220421619e618aeeb5c4340d
Isaque Azevedo <isaque.azevedo@sap.com>
2019-11-22 21:22:57 -0300

    adding everything

---
+++ Assets/Gather.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class Gather : MonoBehaviour
+{
+    public GameObject flexObj;
+    public NVIDIA.Flex.FlexContainer container;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(container);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/NVIDIA/Flex Samples/9. Fluid Renderer/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class NewBehaviourScript2 : MonoBehaviour
+{
+    public GameObject flexObj;
+    public NVIDIA.Flex.FlexContainer container;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(container);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
